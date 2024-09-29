27c379272d30b659c325b8032dec73d68622d94b
Mary <104372970+Mary-clouds@users.noreply.github.com>
2024-09-29 11:29:23 +0200

    added moment for date and time for los angeles element

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let los_angelesElement = document.querySelector("los-angeles");
+let los_angelesElement = los_angelesElement.querySelector("date");
+let los_angelesElement = los_angelesElement.querySelector("time");
+los_angelesElement.innerHTML= moment().format("MMMM Do YYYY");
+los_angelesElement.innerHTML = "11: 39:05 <small>AM</small>";
