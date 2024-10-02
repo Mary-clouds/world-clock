fa917ce887a28e926b40ce0c17d6e941780b399a
Mary <104372970+Mary-clouds@users.noreply.github.com>
2024-10-02 10:21:37 +0200

    changed the element configuration

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
 function cityTimezone(){
-
-
+//updating the city timezone when selected
+// date time format
+let formatDate = format("MMMM Do YYYY");
+let formatTime = format("h:mm:ss [<small>]A[</small]");
 //los angeles
 let los_angelesElement = document.querySelector("los-angeles");
-
 let los_angelesDate = los_angelesElement.querySelector("laDate");
 let los_angelesTime = los_angelesElement.querySelector("laTime");
     //los angeles with moment
 let los_angelesTimezone = moment().tz("America/Los_Angeles");
+    //displaying the LA date in the html
+    los_angelesDate.innerHTML = los_angelesTimezone.formatDate;
+    //displaying the LA time in html
+los_angelesTime.innerHTML = los_angelesTimezone.formatTime;
 //berlin
 let berlinElement = document.querySelector("berlin");
-
 let berlinDate = parisElement.querySelector("berlinDate");
 let berlinTime = parisDate.querySelector("berlinTime");
       //berlin with moment
 let berlinTimezone = moment().tz("Europe/Berlin");
+    //displaying the  berlin date in the html
+ berlinDate.innerHTML = berlinTimezone.formatDate;
+    //displaying the berlin time in html
+ berlinTime.innerHTML = berlinTimezone.formatTime;
 //douala
 let doualaElement = document.querySelector("douala");
-
 let doualaDate = parisElement.querySelector("bdouala");
 let doualaTime = parisDate.querySelector("doualaTime");
      //douala with moment
 let doualaTimezone = moment().tz("Africa/douala");
-// date time format
-let formatDate = format("MMMM Do YYYY");
-let formatTime = format("h:mm:ss [<small>]A[</small]");
-
-//displaying the date in the html
-if(los_angelesElement || berlinElement || doualaElement){ 
-los_angelesDate.innerHTML = los_angelesTimezone.formatDate;
-berlinDate.innerHTML = berlinTimezone.formatDate;
+    //displaying the douala date in the html
 doualaDate.innerHTML = doualaTimezone.formatDate;
-//displaying the time in html
-los_angelesTime.innerHTML = los_angelesTimezone.formatTime;
-berlinTime.innerHTML = berlinTimezone.formatTime;
+    //displaying the douala time in html
 doualaTime.innerHTML = doualaTimezone.formatTime;
-}
+
 
 }
 
